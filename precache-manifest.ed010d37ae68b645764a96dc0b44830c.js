60b323973cf17e96fec64b808e414397f992d348
maizena <pratita.s@mail.ugm.ac.id>
2019-05-22 10:06:47 +0700

    Updates

---
+++ precache-manifest.ed010d37ae68b645764a96dc0b44830c.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b80905b9a8a2be38975c09c1e975eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8735bea8d071623e819",
+    "url": "/static/css/main.7126c593.chunk.css"
+  },
+  {
+    "revision": "12dd2a05ff51bd3ca6b7",
+    "url": "/static/js/2.33be9151.chunk.js"
+  },
+  {
+    "revision": "a8735bea8d071623e819",
+    "url": "/static/js/main.b8a347ff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ac6fd87f9906b4cf8108ae072892ba99",
+    "url": "/static/media/10578641.ac6fd87f.jpg"
+  },
+  {
+    "revision": "a25f46c297573b9e0f0e7f5c2cab4e39",
+    "url": "/static/media/2-770x470.a25f46c2.png"
+  },
+  {
+    "revision": "59b06f52c1900e9d0f07579bc3daefa4",
+    "url": "/static/media/Untitled.59b06f52.png"
+  },
+  {
+    "revision": "72d6bca8947576d2f9608cc6f6cd735a",
+    "url": "/static/media/bahasbahasa.72d6bca8.jpg"
+  },
+  {
+    "revision": "d70c756f2a739722a398819cfa7287a7",
+    "url": "/static/media/bitcoin.d70c756f.png"
+  },
+  {
+    "revision": "01899307116a937c66a0eb4b3927b631",
+    "url": "/static/media/blob.01899307.png"
+  },
+  {
+    "revision": "8d581ffb03c56a0106abc6b330b449d5",
+    "url": "/static/media/cercuri.8d581ffb.png"
+  },
+  {
+    "revision": "62a04e2688d8ba7ede0d823e425cf999",
+    "url": "/static/media/chester-wade.62a04e26.jpg"
+  },
+  {
+    "revision": "2ae3f9ecc812ca146d952de9c8e8c0ba",
+    "url": "/static/media/denys.2ae3f9ec.jpg"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "4fd59ead57f5b7dfa3ca0db01414a256",
+    "url": "/static/media/etherum.4fd59ead.png"
+  },
+  {
+    "revision": "182796a27d309b897e77f616df128522",
+    "url": "/static/media/fabien-bazanegue.182796a2.jpg"
+  },
+  {
+    "revision": "1261b6ef8baab79305e8664eb76dc72e",
+    "url": "/static/media/gamepad.1261b6ef.jpg"
+  },
+  {
+    "revision": "3a749d183532590fda1c14345b6ccca6",
+    "url": "/static/media/mark-finn.3a749d18.jpg"
+  },
+  {
+    "revision": "857c1a3d33280f6d5d386b6e6d0c1aad",
+    "url": "/static/media/notember.857c1a3d.jpg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "43ff45c5dc1f65726b86fc75ea6cabc0",
+    "url": "/static/media/path2.43ff45c5.png"
+  },
+  {
+    "revision": "a70167ea1491987c50049961a9e2e45f",
+    "url": "/static/media/path3.a70167ea.png"
+  },
+  {
+    "revision": "e37326c7e0fd9826c1a35b231340f58b",
+    "url": "/static/media/path4.e37326c7.png"
+  },
+  {
+    "revision": "ceb241f05718fb1311f8810eea959803",
+    "url": "/static/media/path5.ceb241f0.png"
+  },
+  {
+    "revision": "03ea2a0ad05689cebb5a9314a53b8966",
+    "url": "/static/media/patrat.03ea2a0a.png"
+  },
+  {
+    "revision": "0011686ff6ed1468cb12aea8cc8bd0c6",
+    "url": "/static/media/payungapp.0011686f.jpg"
+  },
+  {
+    "revision": "98742a820d2986e78ae284d57deae457",
+    "url": "/static/media/ripp.98742a82.png"
+  },
+  {
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "/static/media/square1.ca257073.png"
+  },
+  {
+    "revision": "6619b9792d7d108bb589b51bbd560278",
+    "url": "/static/media/square2.6619b979.png"
+  },
+  {
+    "revision": "23fa76237f0066f4ffd8f20e77449887",
+    "url": "/static/media/square3.23fa7623.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/static/media/square5.ff004a0e.png"
+  },
+  {
+    "revision": "6a94fc232e10aa0a3c5b13ef1eae61d3",
+    "url": "/static/media/undraw_adventure_4hum.6a94fc23.svg"
+  },
+  {
+    "revision": "11fa5f73cb719f0f1a38e02e47bdc843",
+    "url": "/static/media/waves.11fa5f73.png"
+  }
+]);
